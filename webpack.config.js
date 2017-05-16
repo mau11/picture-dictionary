372ea2fa25a2d2e00c48a810a228f0877696a839
Cai Lu <cailinglu@outlook.com>
2017-05-15 19:58:52 -0700

    Set up Gulp and Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,16 @@
+const path = require('path');
 
+module.exports = {
+  entry: path.resolve(__dirname, 'app/index.ts'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.ts$/, exclude: /node_modules/, loader: [ 'awesome-typescript-loader' ] }
+    ]
+  }
+};
